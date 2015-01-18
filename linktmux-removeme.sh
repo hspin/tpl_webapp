ba82aa344d2b6c9fd1707cb8ea4edff0be4c3fc4
GS Mercury <me.gsbit@hsx5.com>
2015-01-17 21:25:05 -0500

    symbolic linking fix

--- linktmux-removeme.sh
+++ linktmux-removeme.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DIR=`pwd`
+DIR=$(pwd)
 HNAME=${PWD##*/} 
 
-sed -i '' 's/uno11/$HNAME/' .general-tmuxinator.yml
+sed -i "s/uno11/$HNAME/" .general-tmuxinator.yml
 
-sed -i '' 's/dos22/$DIR/' .general-tmuxinator.yml
+sed -i "s|dos22|$DIR|" .general-tmuxinator.yml
 
-ln -s $DIR/.general-tmuxinator.yml $HOME/.tmuxinator/$DIR.yml
+ln -s $DIR/.general-tmuxinator.yml $HOME/.tmuxinator/$HNAME.yml
 
 echo "tmuxinator is ready"
