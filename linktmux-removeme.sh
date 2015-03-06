4d63d51df10b8efe450dd745fe9c803a03643ac4
Gerry Salazar <gs@hspindesign.com>
2015-03-06 12:25:14 -0500

    fixing paths and namings

--- linktmux-removeme.sh
+++ linktmux-removeme.sh
@@ -5,7 +5,7 @@ HNAME=${PWD##*/}
 
 sed -i "s/uno11/$HNAME/" .general-tmuxinator.yml
 
-sed -i "s|dos22|$DIR|" .general-tmuxinator.yml
+sed -i "s|dos22|$HNAME|" .general-tmuxinator.yml
 
 ln -s $DIR/.general-tmuxinator.yml $HOME/.tmuxinator/$HNAME.yml
 
