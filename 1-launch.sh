ae310bdd2648cef716a74c3c9576449a7c6e8c29
GS Mercury <me.gsbit@hsx5.com>
2015-03-06 18:39:43 -0500

    adding individual launch scripts

---
+++ 1-launch.sh
@@ -0,0 +1,1 @@
+docker run --rm -e="TERM=screen-256color" -v $(pwd)/.sessions/:/home/uservim/.sessions -v $(pwd)/NAME:/home/uservim/code/ -it me_vim
---
+++ 2-launch.sh
@@ -0,0 +1,1 @@
+docker run --rm -p 9000:9000 -p 8001:8001 -p 3000:3000 -p 35729:35729 -v $(pwd)/NAME:/home/deployer/code/ -it DNAME
